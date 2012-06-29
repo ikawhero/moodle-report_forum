bf10ace6f9221b4b006f1f1067fc973594984d2c
Shane Elliott <shane@pukunui.com>
2012-06-29 11:56:40 +0800

    Code Point: Course Report

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2012051000;
+$plugin->requires  = 2011110200;     // Requires this Moodle version
+$plugin->component = 'report_forum'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_BETA;  // Plugin maturity
+$plugin->release   = '2.2';          // Version of this plugin release
