1d94ed482317fb0b4bac8b559526304f73786abd
Shane Elliott <shane@pukunui.com>
2012-06-29 12:00:05 +0800

    Code Point: Code Comments

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Forum report
+ *
+ * @package    report
+ * @subpackage forum
+ * @copyright  2012 Michael de Raadt <michaeld@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+// Required files
 require_once('../../config.php');
 
 echo 'Course Report';
